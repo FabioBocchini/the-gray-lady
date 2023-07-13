ccbb2425af0a19a8921e1f804ca1e314170c106b
fabiobocchini <iambiozzus@gmail.com>
2023-07-13 12:13:56 +0200

    feat: moved number of message in conversation to variable

--- src/utils/apiClient.ts
+++ src/utils/apiClient.ts
@@ -1,10 +1,12 @@
 import { Conversation } from '@/types/conversation'
 
+const numberOfMessagesInConversation = 12
+
 export async function sendQuestionToAI(
   question: string,
   conversation: Conversation
 ): Promise<string> {
-  const trimmedConversation = conversation.slice(-12)
+  const trimmedConversation = conversation.slice(-numberOfMessagesInConversation)
   const res = await fetch(`/api/chat`, {
     method: 'POST',
     body: JSON.stringify({ question, conversation: JSON.stringify(trimmedConversation) })
